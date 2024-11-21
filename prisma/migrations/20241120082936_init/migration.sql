dc29502fd551cc71b4b03844c538c23abda2957c
mengeroshi <dpllgr@hotmail.com>
2024-11-20 18:17:41 -0600

    migrations generated

---
+++ prisma/migrations/20241120082936_init/migration.sql
@@ -0,0 +1,161 @@
+-- CreateEnum
+CREATE TYPE "ContentType" AS ENUM ('VIDEO', 'ARTICLE', 'CODE');
+
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('LOW', 'MEDIUM', 'HARD');
+
+-- CreateTable
+CREATE TABLE "Album" (
+    "id" SERIAL NOT NULL,
+    "companyId" INTEGER,
+    "contentInfoId" INTEGER NOT NULL,
+
+    CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ContentInfo" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL,
+    "imgUrl" TEXT,
+    "url" TEXT,
+
+    CONSTRAINT "ContentInfo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" SERIAL NOT NULL,
+    "contentInfoId" INTEGER NOT NULL,
+
+    CONSTRAINT "Playlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" SERIAL NOT NULL,
+    "typeContent" "ContentType" NOT NULL,
+    "difficulty" "Difficulty" NOT NULL DEFAULT 'LOW',
+    "albumId" INTEGER,
+    "companyId" INTEGER,
+    "contentInfoId" INTEGER NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToAlbum" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPlaylist" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToSong" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_PlaylistToSong" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_slug_key" ON "Company"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContentInfo_slug_key" ON "ContentInfo"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToAlbum_AB_unique" ON "_CategoryToAlbum"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToAlbum_B_index" ON "_CategoryToAlbum"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPlaylist_AB_unique" ON "_CategoryToPlaylist"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPlaylist_B_index" ON "_CategoryToPlaylist"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToSong_AB_unique" ON "_CategoryToSong"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToSong_B_index" ON "_CategoryToSong"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PlaylistToSong_AB_unique" ON "_PlaylistToSong"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistToSong_B_index" ON "_PlaylistToSong"("B");
+
+-- AddForeignKey
+ALTER TABLE "Album" ADD CONSTRAINT "Album_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Album" ADD CONSTRAINT "Album_contentInfoId_fkey" FOREIGN KEY ("contentInfoId") REFERENCES "ContentInfo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Playlist" ADD CONSTRAINT "Playlist_contentInfoId_fkey" FOREIGN KEY ("contentInfoId") REFERENCES "ContentInfo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Song" ADD CONSTRAINT "Song_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "Album"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Song" ADD CONSTRAINT "Song_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Song" ADD CONSTRAINT "Song_contentInfoId_fkey" FOREIGN KEY ("contentInfoId") REFERENCES "ContentInfo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToAlbum" ADD CONSTRAINT "_CategoryToAlbum_A_fkey" FOREIGN KEY ("A") REFERENCES "Album"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToAlbum" ADD CONSTRAINT "_CategoryToAlbum_B_fkey" FOREIGN KEY ("B") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPlaylist" ADD CONSTRAINT "_CategoryToPlaylist_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPlaylist" ADD CONSTRAINT "_CategoryToPlaylist_B_fkey" FOREIGN KEY ("B") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToSong" ADD CONSTRAINT "_CategoryToSong_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToSong" ADD CONSTRAINT "_CategoryToSong_B_fkey" FOREIGN KEY ("B") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_A_fkey" FOREIGN KEY ("A") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_B_fkey" FOREIGN KEY ("B") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
