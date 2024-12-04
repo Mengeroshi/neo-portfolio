8b0d42639b96192e4fbde8a1c361c6fa5e0cba2a
mengeroshi <dpllgr@hotmail.com>
2024-12-04 01:03:06 -0600

    Enhance TextInput component to support textarea input

--- src/components/Forms/TextInput.tsx
+++ src/components/Forms/TextInput.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 
-type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+type TextInputProps = React.InputHTMLAttributes<
+  HTMLInputElement | HTMLTextAreaElement
+> & {
   label: string;
+  isTextArea?: boolean;
 };
 
-export const TextInput: React.FC<TextInputProps> = ({ label, ...props }) => {
+export const TextInput: React.FC<TextInputProps> = ({
+  label,
+  isTextArea = false,
+  ...props
+}) => {
   return (
     <label className="block w-full">
       <div className="w-fit bg-blue-900 px-2 font-semibold">{label}</div>
-      <input
-        className="w-full border border-blue-900 bg-blue-200 px-2 py-1 text-blue-900 outline-none"
-        {...props}
-      />
+      {isTextArea ? (
+        <textarea
+          aria-multiline
+          className="w-full border border-blue-900 bg-blue-200 px-2 py-1 text-blue-900 outline-none"
+          {...props}
+        />
+      ) : (
+        <input
+          className="w-full border border-blue-900 bg-blue-200 px-2 py-1 text-blue-900 outline-none"
+          {...props}
+        />
+      )}
     </label>
   );
 };
