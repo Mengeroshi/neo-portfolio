4ab27673a65f8ccd8c5a109ade1888074862401d
mengeroshi <dpllgr@hotmail.com>
2024-12-17 02:22:24 -0600

    Modal component updated

--- src/components/Modals/Modal.tsx
+++ src/components/Modals/Modal.tsx
@@ -7,9 +7,11 @@ import ReactDOM from "react-dom";
 
 export const Modal = ({
   children,
+  title,
 }: {
   children: React.ReactNode;
   className?: string;
+  title?: string;
 }) => {
   const router = useRouter();
   const dialogRef = useRef<ElementRef<"dialog">>(null);
@@ -33,20 +35,21 @@ export const Modal = ({
   return ReactDOM.createPortal(
     <dialog
       ref={dialogRef}
-      className="z-100 absolute bottom-0 left-0 right-0 top-0"
+      className="z-100 absolute bottom-0 left-0 right-0 top-0 max-h-screen overflow-hidden"
       onClose={onDismiss}
       onClick={onDismiss}
     >
       <article
-        className="relative border border-[#3ECFDE] bg-black p-8"
+        className="relative flex max-h-screen flex-col border border-[#3ECFDE] bg-black p-8"
         onClick={(event) => {
           event.stopPropagation();
         }}
       >
+        {title && <h1 className="mb-4 text-4xl text-blue-900">{title}</h1>}
         {children}
         <button
           onClick={onDismiss}
-          className="absolute right-0 top-0 h-fit w-fit p-0.5"
+          className="absolute right-0 top-0 h-fit w-fit border border-r-0 border-t-0 border-blue-900 p-0.5"
         >
           <CloseIcon className="size-7 text-[#3ECFDE]" />
         </button>
