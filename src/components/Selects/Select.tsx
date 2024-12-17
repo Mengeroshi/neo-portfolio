a1ed281e1308ff8542d8de62130565cd5a5e2966
mengeroshi <dpllgr@hotmail.com>
2024-12-16 23:52:04 -0600

    Select props updated

--- src/components/Selects/Select.tsx
+++ src/components/Selects/Select.tsx
@@ -9,11 +9,15 @@ export const Select = ({
   placeholder,
   isMulti,
   options,
+  labelField,
+  valueField,
 }: {
   label: string;
   placeholder?: string;
   isMulti?: boolean;
   options: TOption[];
+  labelField: string;
+  valueField: string;
 }) => {
   return (
     <Label name={label}>
@@ -22,8 +26,8 @@ export const Select = ({
         className="min-h-[unset] !border-blue-900 bg-blue-200 !px-2 !py-1 text-base text-blue-900 !outline-none [&_.react-dropdown-select-dropdown]:border-blue-900 [&_.react-dropdown-select-dropdown]:bg-blue-200 [&_.react-dropdown-select-item-selected]:border-blue-900 [&_.react-dropdown-select-item-selected]:!bg-blue-900 [&_.react-dropdown-select-item-selected]:!text-blue-200 [&_.react-dropdown-select-item]:!border-blue-900 [&_.react-dropdown-select-option-remove]:hover:!text-blue-200 [&_.react-dropdown-select-option]:bg-blue-900 [&_.react-dropdown-select-option]:text-blue-200 [&_.react-dropdown-select-option]:!opacity-100"
         placeholder={placeholder}
         options={options}
-        labelField="name"
-        valueField="id"
+        labelField={labelField}
+        valueField={valueField}
         values={[]}
         onChange={(value) => console.log({ value })}
       />
