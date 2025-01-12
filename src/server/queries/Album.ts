77152f9ae9cdd7a7c3400b1524ea80f617875222
mengeroshi <dpllgr@hotmail.com>
2025-01-11 23:56:27 -0600

    Add getAlbumBySlug query

--- src/server/queries/Album.ts
+++ src/server/queries/Album.ts
@@ -16,3 +16,34 @@ export const getAlbumsCases = async () => {
     },
   });
 };
+
+export const getAlbumBySlug = async (slug: string) => {
+  return await db.album.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      ContentInfo: {
+        select: {
+          name: true,
+          createdAt: true,
+          imgUrl: true,
+        },
+      },
+      Songs: {
+        select: {
+          id: true,
+          ContentInfo: {
+            select: {
+              name: true,
+              description: true,
+              createdAt: true,
+              imgUrl: true,
+            },
+          },
+        },
+      },
+    },
+  });
+};
