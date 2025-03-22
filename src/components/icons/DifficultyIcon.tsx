2fa3edbec861ae67ffcacdfc3d0313e91fdf9a5b
mengeroshi <dpllgr@hotmail.com>
2025-03-22 01:43:12 -0600

    Difficulty Icon colors updated

--- src/components/icons/DifficultyIcon.tsx
+++ src/components/icons/DifficultyIcon.tsx
@@ -9,7 +9,7 @@ const Triangle = () => (
     viewBox="0 0 23 19"
   >
     <path
-      className="stroke-blue-900 group-hover:fill-green-200 group-hover:stroke-green-900"
+      className="stroke-blue-900/60 group-hover:fill-green-200 group-hover:stroke-green-900"
       d="m11.5 1 9.96 17.25H1.54z"
     ></path>
   </svg>
@@ -24,7 +24,7 @@ const Square = () => (
     viewBox="0 0 22 21"
   >
     <path
-      className="stroke-blue-900 group-hover:fill-yellow-200 group-hover:stroke-yellow-900"
+      className="stroke-blue-900/60 group-hover:fill-yellow-200 group-hover:stroke-yellow-900"
       d="M1 1h20v19H1z"
     ></path>
   </svg>
@@ -39,7 +39,7 @@ const Hexagon = () => (
     viewBox="0 0 24 26"
   >
     <path
-      className="stroke-blue-900 group-hover:fill-red-200 group-hover:stroke-red-900"
+      className="stroke-blue-900/60 group-hover:fill-red-200 group-hover:stroke-red-900"
       d="m12 1 10.392 6v12L12 25 1.608 19V7z"
     ></path>
   </svg>
