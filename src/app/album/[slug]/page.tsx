0a266eec1409c7bd123ce302392d9da063163b44
mengeroshi <dpllgr@hotmail.com>
2025-04-23 01:20:43 -0600

    Album page mvp

--- src/app/album/[slug]/page.tsx
+++ src/app/album/[slug]/page.tsx
@@ -1,26 +1,11 @@
+import { Badge } from "@/components/Badges/Badge";
+import { LabelWithUnderLine } from "@/components/Forms/LabelWithUnderline";
 import { PixeledImg } from "@/components/Images/PixeledImg";
 import { SongsTable } from "@/components/Tables/SongsTable";
 import { IMGURLPLACEHOLDER } from "@/constants";
 import { getAlbumBySlug } from "@/server/queries/Album";
 import { LaunchIcon } from "@sanity/icons";
 import Link from "next/link";
-
-const LabelWithUnderLine = ({
-  label,
-  children,
-}: {
-  label: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <div>
-      <div className="border-b border-blue-900/40 text-sm leading-none tracking-widest text-blue-900/40">
-        {label}
-      </div>
-      {children}
-    </div>
-  );
-};
 
 export type TTSongTable = Pick<
   NonNullable<Awaited<ReturnType<typeof getAlbumBySlug>>>,
@@ -41,7 +26,9 @@ export default async function Page({
   }
 
   const {
-    ContentInfo: { imgUrl, name, description, url },
+    ContentInfo: { imgUrl, name, description, url, createdAt },
+    Categories,
+    Company,
   } = album;
 
   return (
@@ -56,10 +43,10 @@ export default async function Page({
           height={300}
           className="h-[300px] w-[300px] border-r border-blue-900/60"
         />
-        <div className="flex flex-col gap-1 p-4 text-blue-900">
+        <div className="flex w-full flex-col gap-1 p-4 text-blue-900">
           <LabelWithUnderLine label="name">
             {
-              <h1 className="text-4xl font-normal tracking-wide">
+              <h1 className="gap-2 text-4xl font-normal tracking-wide">
                 {url ? (
                   <Link
                     className="group flex hover:underline"
@@ -67,6 +54,7 @@ export default async function Page({
                     target="_blank"
                   >
                     {name}
+
                     <LaunchIcon className="hidden group-hover:block" />
                   </Link>
                 ) : (
@@ -76,10 +64,38 @@ export default async function Page({
             }
           </LabelWithUnderLine>
           <LabelWithUnderLine label="description">
-            <p className="text-justify font-light tracking-wider">
+            <p className="max-h-[120px] overflow-scroll text-justify font-light tracking-wider">
               {description}
             </p>
           </LabelWithUnderLine>
+          <div className="mt-auto flex gap-4">
+            <LabelWithUnderLine label="categories">
+              <div className="flex gap-1">
+                {Categories.length > 0 &&
+                  Categories.map((category) => (
+                    <Badge
+                      color={"red"}
+                      text={category.name}
+                      key={category.id}
+                    />
+                  ))}
+              </div>
+            </LabelWithUnderLine>
+            <LabelWithUnderLine label="company">
+              {Company && <Badge text={Company.name} />}
+            </LabelWithUnderLine>
+            <LabelWithUnderLine label=" creation date">
+              <time
+                className="mt-1 text-sm font-light leading-none tracking-wider text-blue-900"
+                dateTime={createdAt.toISOString()}
+              >
+                {new Date().toLocaleString("en-US", {
+                  month: "long",
+                  year: "numeric",
+                })}
+              </time>
+            </LabelWithUnderLine>
+          </div>
         </div>
       </header>
       <SongsTable songs={album.Songs} />
