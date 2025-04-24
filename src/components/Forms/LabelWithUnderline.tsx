1ce6d1f963be1bb35d7d0ab53588dcdc6a545ba3
mengeroshi <dpllgr@hotmail.com>
2025-04-23 18:41:42 -0600

    Label  with underline fixed

--- src/components/Forms/LabelWithUnderline.tsx
+++ src/components/Forms/LabelWithUnderline.tsx
@@ -1,16 +1,28 @@
+import { twMerge } from "tailwind-merge";
+
 export const LabelWithUnderLine = ({
   label,
   children,
+  classNames,
 }: {
   label: string;
   children: React.ReactNode;
+  classNames?: {
+    label?: string;
+    children?: string;
+  };
 }) => {
   return (
     <div className="flex h-fit w-full flex-col">
-      <div className="mb-1 border-b border-blue-900/40 text-sm leading-none tracking-widest text-blue-900/40">
+      <div
+        className={twMerge(
+          "mb-1 border-y border-blue-900/40 bg-blue-200 px-4 text-sm leading-none tracking-widest text-blue-900/40",
+          classNames?.label,
+        )}
+      >
         {label}
       </div>
-      {children}
+      <div className={twMerge("px-4", classNames?.children)}>{children}</div>
     </div>
   );
 };
